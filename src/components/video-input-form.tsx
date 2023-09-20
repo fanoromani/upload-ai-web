b0e3bb0aa536b8a4ab01f989f8a7ad777657504b
Stefano Romani <fanoromani@gmail.com>
2023-09-19 23:12:03 -0300

    added youtube download

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -1,12 +1,14 @@
-import { FileVideo, Upload } from "lucide-react";
+import { FileVideo, Upload, Youtube } from "lucide-react";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "./ui/tabs";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { loadFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { api } from "@/lib/axios";
+import { Input } from "./ui/input";
 
 type Status = "waiting" | "converting" | "uploading" | "generating" | "success";
 interface VideoInputFormProps {
@@ -20,10 +22,14 @@ const statusMessages = {
   success: "Success!",
 };
 
-export function VideoInputForm(props: VideoInputFormProps) {
+export function VideoInputForm({ onVideoUploaded }: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null);
+  const [uploadFormat, setUploadFormat] = useState<"youtube" | "upload">(
+    "youtube"
+  );
   const [status, setStatus] = useState<Status>("waiting");
   const promptInputRef = useRef<HTMLTextAreaElement>(null);
+  const urlInputRef = useRef<HTMLInputElement>(null);
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget;
@@ -38,26 +44,38 @@ export function VideoInputForm(props: VideoInputFormProps) {
   async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const prompt = promptInputRef.current?.value;
+    const data = new FormData();
+    let videoID = "";
 
-    if (!videoFile) {
-      return;
+    if (uploadFormat === "upload") {
+      if (!videoFile) {
+        return;
+      }
+
+      setStatus("converting");
+
+      const audioFile = await convertVideoToAudio(videoFile);
+
+      data.append("file", audioFile);
+      setStatus("uploading");
+      const response = await api.post("/videos", data);
+      videoID = response.data.video.id;
+    } else {
+      const youtubeURL = urlInputRef.current?.value;
+      setStatus("converting");
+
+      setStatus("uploading");
+      const response = await api.post("/youtube", { url: youtubeURL, prompt });
+      console.log(response);
+      videoID = response.data;
     }
 
-    setStatus("converting");
-
-    const audioFile = await convertVideoToAudio(videoFile);
-
-    const data = new FormData();
-    data.append("file", audioFile);
-    setStatus("uploading");
-    const response = await api.post("/videos", data);
-    const videoID = response.data.video.id;
     setStatus("generating");
     await api.post(`/videos/${videoID}/transcription`, {
       prompt,
     });
     setStatus("success");
-    props.onVideoUploaded(videoID);
+    onVideoUploaded(videoID);
   }
 
   async function convertVideoToAudio(video: File) {
@@ -97,33 +115,65 @@ export function VideoInputForm(props: VideoInputFormProps) {
 
   return (
     <form onSubmit={handleUploadVideo} className="space-y-4">
-      <label
-        htmlFor="video"
-        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-      >
-        {previewURL ? (
-          <video
-            src={previewURL}
-            controls={false}
-            className="absolute inset-0 pointer-events-none"
-          ></video>
-        ) : (
-          <>
-            <FileVideo className="w-4 h-4" />
-            Select video
-          </>
-        )}
-      </label>
-      <input
-        type="file"
-        id="video"
-        accept="video/mp4"
-        className="sr-only"
-        onChange={handleFileSelected}
-      />
+      <Label>Select Input Format</Label>
+      <Tabs defaultValue="Youtube">
+        <TabsList className="flex justify-around">
+          <TabsTrigger
+            value="youtube"
+            className="flex-1"
+            onClick={() => setUploadFormat("youtube")}
+          >
+            <Youtube className="w-4 h-4 mr-2" />
+            Youtube
+          </TabsTrigger>
+          <TabsTrigger
+            value="upload"
+            className="flex-1"
+            onClick={() => setUploadFormat("upload")}
+          >
+            <Upload className="w-4 h-4 mr-2" />
+            Upload
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="youtube" className="space-y-2">
+          <Label>Video URL</Label>
+          <Input
+            type="url"
+            ref={urlInputRef}
+            disabled={status !== "waiting"}
+          ></Input>
+        </TabsContent>
+        <TabsContent value="upload">
+          <Label
+            htmlFor="video"
+            className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+          >
+            {previewURL ? (
+              <video
+                src={previewURL}
+                controls={false}
+                className="absolute inset-0 pointer-events-none"
+              />
+            ) : (
+              <>
+                <FileVideo className="w-4 h-4" />
+                Select Video
+              </>
+            )}
+          </Label>
+          <input
+            type="file"
+            id="video"
+            accept="video/mp4"
+            className="sr-only"
+            onChange={handleFileSelected}
+          />
+        </TabsContent>
+      </Tabs>
+
       <Separator />
       <div className="space-y-2">
-        <Label htmlFor="transcription-prompt">Transcription prompt</Label>
+        <Label htmlFor="transcription-prompt">Transcription Prompt</Label>
         <Textarea
           disabled={status !== "waiting"}
           ref={promptInputRef}
